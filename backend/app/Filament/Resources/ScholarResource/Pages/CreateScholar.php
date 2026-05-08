2ecbccc50be2604f50d1fa1b47814e7bfa4941de
LeeeZoon <rehanzulhilmibudiman@gmail.com>
2026-05-08 14:01:39 +0700

    backend

--- backend/app/Filament/Resources/ScholarResource/Pages/CreateScholar.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace App\Filament\Resources\ScholarResource\Pages;
-use App\Filament\Resources\ScholarResource;
-use Filament\Resources\Pages\CreateRecord;
-class CreateScholar extends CreateRecord
-{
-    protected static string $resource = ScholarResource::class;
-}
